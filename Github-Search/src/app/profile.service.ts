1214157119df53fcaa08ae59d869f332d7327715
AkinyiOwuor <owuorrachel@gmail.com>
2020-04-13 10:57:01 +0300

    add profile service

--- Github-Search/src/app/profile.service.ts
+++ Github-Search/src/app/profile.service.ts
@@ -1,26 +1,26 @@
-// import { Injectable } from '@angular/core';
-// import { HttpClient }from '@angular/common/http';
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class ProfileService {
+import { Injectable } from '@angular/core';
+import { HttpClient }from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
 
-//    private username:string;
-//    private client_id = "0ddd798ddad906269c18";
-//    private client_secret = " 91f04505724adfb30fb971d543cdb21688bf139c";
+   private username:string;
+   private client_id = "0ddd798ddad906269c18";
+   private client_secret = " 91f04505724adfb30fb971d543cdb21688bf139c";
 
-//   constructor(private http:HttpClient) { 
-//     console.log("Service is now ready!");
-//     this.username = "AkinyiOwuor";
-//   }
-//   getProfileInfo(){
-//     return this.http.get("https://api.github.com/users/" + this.username + "?client_id="+ this.client_id + "&client_secret=" + this.client_secret)
-//   }
+  constructor(private http:HttpClient) { 
+    console.log("Service is now ready!");
+    this.username = "AkinyiOwuor";
+  }
+  getProfileInfo(){
+    return this.http.get("https://api.github.com/users/" + this.username + "?client_id="+ this.client_id + "&client_secret=" + this.client_secret)
+  }
 
-//   getProfileRepos(){
-//     return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id="+ this.client_id + "&client_secret=" + this.client_secret)
-//   }
-//   updateProfile(username:string){
-//     this.username = username;
-//   }
-// }
+  getProfileRepos(){
+    return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id="+ this.client_id + "&client_secret=" + this.client_secret)
+  }
+  updateProfile(username:string){
+    this.username = username;
+  }
+}
